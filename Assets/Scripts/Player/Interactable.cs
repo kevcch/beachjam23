a76a50c213cde07b78a3d5de824ae7cfc05faa0d
Jasmine Li <jasmine@nochistudios.com>
2023-07-19 14:44:04 -0400

    fixed terrain, interaction scripts

--- Assets/Scripts/Player/Interactable.cs
+++ Assets/Scripts/Player/Interactable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interaction : MonoBehaviour
+public class Interactable : MonoBehaviour
 {
     private Collider col;
 
