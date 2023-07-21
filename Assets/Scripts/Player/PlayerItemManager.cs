24b69a5646c854787c986348761f485b94dfac22
Kevin Choong <kevincc@umich.edu>
2023-07-21 23:44:24 +0800

    Implement item dropping

--- Assets/Scripts/Player/PlayerItemManager.cs
+++ Assets/Scripts/Player/PlayerItemManager.cs
@@ -8,7 +8,17 @@ public class PlayerItemManager: MonoBehaviourPun
     [SerializeField] public List<ItemClass> itemList = new List<ItemClass>();
     public List<GameObject> stackedItems;
     public Transform stackedItemsTransform;
+    public GameObject droppedItemPrefab;
 
+    private void Update()
+    {
+        if (base.photonView.IsMine) {
+            if (itemList.Count > 0 && Input.GetKeyDown(KeyCode.Q)) {
+                DropItem();
+            }
+        }
+        
+    }
     public void AddItem(ItemClass iso)
     {
         Debug.Log(iso.objectViewPrefab + " " + iso.objectType);
@@ -39,13 +49,24 @@ public class PlayerItemManager: MonoBehaviourPun
             Debug.Log("Error: Tried to remove items when none left to remove");
             return;
         }
-        itemList.RemoveAt(itemList.Count - 1);
         Destroy(stackedItems[itemList.Count - 1]);
         stackedItems.RemoveAt(itemList.Count - 1);
+        itemList.RemoveAt(itemList.Count - 1);
     }
 
     public void DropItem() {
+        if (GetNumItems() <= 0)
+        {
+            Debug.Log("Error: Tried to drop items when none left to drop");
+            return;
+        }
+        ItemClass item = itemList[itemList.Count - 1];
+        GameObject droppedItem = Instantiate(droppedItemPrefab, stackedItemsTransform.position, stackedItemsTransform.rotation);
+        droppedItem.GetComponent<DroppedItem>().item = new ItemClass(item.objectViewPrefab, item.objectType);
+        GameObject droppedItemView = Instantiate(droppedItem.GetComponent<DroppedItem>().item.objectViewPrefab, stackedItemsTransform.position, stackedItemsTransform.rotation);
+        droppedItemView.transform.parent = droppedItem.transform;
 
+        RemoveItem();
     }
 
     public int GetNumItems() {
