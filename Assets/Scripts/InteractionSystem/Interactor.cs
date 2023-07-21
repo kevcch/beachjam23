718f545f4e8eb4c6b7d3735ba63f7de57ab1ba8b
Kevin Choong <kevincc@umich.edu>
2023-07-21 09:58:55 +0800

    Interaction UI popup

--- Assets/Scripts/InteractionSystem/Interactor.cs
+++ Assets/Scripts/InteractionSystem/Interactor.cs
@@ -9,10 +9,12 @@ public class Interactor : MonoBehaviourPun
     [SerializeField] private Transform _interactionPoint;
     [SerializeField] private float _interactionPointRadius = 0.5f;
     [SerializeField] LayerMask _interactableMask;
+    [SerializeField] private InteractionPromptUI _interactionPromptUI;
 
     private readonly Collider[] _colliders = new Collider[3];
     [SerializeField] private int _numFound;
 
+    private IInteractable _interactable;
     void Update()
     {
         if (base.photonView.IsMine) {
@@ -20,11 +22,23 @@ public class Interactor : MonoBehaviourPun
             _colliders, _interactableMask);
             if (_numFound > 0)
             {
-                var interactable = _colliders[0].GetComponent<IInteractable>();
-                if (interactable != null && Input.GetKeyDown(KeyCode.E)) {
-                    interactable.Interact(this);
+                _interactable = _colliders[0].GetComponent<IInteractable>();
+                if (_interactable != null)
+                {
+                    if (!_interactionPromptUI.IsDisplayed) _interactionPromptUI.SetUp(_interactable.InteractionPrompt);
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        _interactable.Interact(this);
+
+                    }
                 }
+
             }
+            else {
+                if (_interactable != null) _interactable = null;
+                if (_interactionPromptUI.IsDisplayed) _interactionPromptUI.Close();
+            }
+
         }
             
     }
